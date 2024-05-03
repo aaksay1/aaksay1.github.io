cbd20ef10c320c8ed2d330d12ceeb0fbe801fb22
Akin Aksay <akinaksay1@gmail.com>
2024-05-03 19:11:28 -0400

    fixed form

--- email-form.js
+++ email-form.js
@@ -1,17 +1,11 @@
-// Initialize EmailJS with your user ID
-(function(){
-    emailjs.init("NeIEKz6yxBgE57cwP"); // Replace 'your-user-id' with your actual EmailJS user ID
-})();
-
-// Add event listener to the form
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
 
-    // Send the form data using EmailJS
     emailjs.sendForm('service_hdk75jd', 'template_8105i7z', this)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             alert('Email successfully sent!');
+            document.getElementById('contact-form').reset();
         }, function(error) {
             console.log('FAILED...', error);
             alert('Failed to send email: ' + JSON.stringify(error));
